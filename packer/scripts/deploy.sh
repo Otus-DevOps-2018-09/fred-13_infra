e98f74a53b721943ff2213b242bbaa68bbcbd626
Alfred Meshcheraykov <ameshcheryakov@plazius.ru>
2018-10-19 17:58:41 +0300

    Add scripts for dir packer/scripts/

---
+++ packer/scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Копируем код приложения
+git clone -b monolith https://github.com/express42/reddit.git
+# Переходим в директорию проекта и устанавливаем зависимости приложения
+cd reddit && bundle install
+# Запускаем сервер приложения в папке проект
+puma -d
