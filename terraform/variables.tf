4afd9cab52bfe4f0c4d8bbb3588d8f63bbd8b5fc
Alfred Meshcheraykov <ameshcheryakov@plazius.ru>
2018-10-27 15:17:09 +0300

    Added new variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,25 @@
 variable project {
   description = "Project ID"
 }
+
 variable region {
   description = "Region"
-  default = "europe-west1"
+  default     = "europe-west1"
 }
+
 variable public_key_path {
-  description = "Path to the public key used for ssh access"
+  description = "Path to the PUBLIC key used for ssh access"
 }
+
 variable disk_image {
   description = "Disk image"
+}
+
+variable private_key_path {
+  description = "Path to the PRIVITE key used for ssh access"
+}
+
+variable zone_app {
+  description = "Zone"
+  default     = "europe-west1-b"
 }
