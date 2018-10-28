437394de6fac35386638d3b9d7bf4de20e0328b4
Alfred Meshcheraykov <ameshcheryakov@plazius.ru>
2018-10-28 16:01:06 +0300

    Added to Terraform-2

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,10 @@ resource "google_compute_instance" "app" {
     # сеть, к которой присоединить данный интерфейс
     network = "default"
 
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
+
     # использовать ephemeral IP для доступа из Интернет
     access_config {}
   }
@@ -65,6 +69,22 @@ resource "google_compute_firewall" "firewall_puma" {
 
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
+}
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "default-allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
 }
 
 resource "google_compute_project_metadata" "ssh-keys" {
